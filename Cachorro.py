6a4284ca2e563bfcb36be0bd645be8fdead6bddd
Luis Daniel <96265875+Luisdaniel1909@users.noreply.github.com>
2022-12-09 00:42:54 -0600

    Update Cachorro.py

--- Cachorro.py
+++ Cachorro.py
@@ -10,16 +10,16 @@ from pybricks.tools import wait, StopWatch
 
 
 class Puppy:
-    # These constants are used for positioning the legs.
+    # Estas constantes se utilizan para posicionar las piernas.
     HALF_UP_ANGLE = 25
     STAND_UP_ANGLE = 65
     STRETCH_ANGLE = 125
 
-    # These constants are for positioning the head.
+    # Estas constantes son para posicionar la cabeza.
     HEAD_UP_ANGLE = 0
     HEAD_DOWN_ANGLE = -40
 
-    # These constants are for the eyes.
+    # Estas constantes son para los ojos.
     NEUTRAL_EYES = Image(ImageFile.NEUTRAL)
     TIRED_EYES = Image(ImageFile.TIRED_MIDDLE)
     TIRED_LEFT_EYES = Image(ImageFile.TIRED_LEFT)
@@ -28,65 +28,66 @@ class Puppy:
     HURT_EYES = Image(ImageFile.HURT)
     ANGRY_EYES = Image(ImageFile.ANGRY)
     HEART_EYES = Image(ImageFile.LOVE)
-    SQUINTY_EYES = Image(ImageFile.TEAR)  # the tear is erased later
+    SQUINTY_EYES = Image(ImageFile.TEAR)  # la lágrima se borra después
 
     def __init__(self):
-        # Initialize the EV3 brick.
+        # Inicializar el ladrillo EV3.
         self.ev3 = EV3Brick()
 
-        # Initialize the motors connected to the back legs.
+        # Inicializar los motores conectados a las patas traseras.
         self.left_leg_motor = Motor(Port.D, Direction.COUNTERCLOCKWISE)
         self.right_leg_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
 
-        # Initialize the motor connected to the head.
-        # Worm gear moves 1 tooth per rotation. It is interfaced to a 24-tooth
-        # gear. The 24-tooth gear is connected to parallel 12-tooth gears via
-        # an axle. The 12-tooth gears interface with 36-tooth gears.
+        #Inicializar el motor conectado al cabezal.
+        #El tornillo sin fin mueve 1 diente por rotación. 
+        #Está conectado a un engranaje de 24 dientes. 
+        #El engranaje de 24 dientes está conectado a engranajes paralelos 
+        #de 12 dientes a través de un eje. Los engranajes de 12 dientes se conectan 
+        #con engranajes de 36 dientes.
         self.head_motor = Motor(Port.C, Direction.COUNTERCLOCKWISE,
                                 gears=[[1, 24], [12, 36]])
 
-        # Initialize the Color Sensor. It is used to detect the colors when
-        # feeding the puppy.
+        #Inicialice el Sensor de Color. 
+        #Se utiliza para detectar los colores al alimentar al cachorro.
         self.color_sensor = ColorSensor(Port.S4)
 
-        # Initialize the touch sensor. It is used to detect when someone pets
-        # the puppy.
+        #Inicializa el sensor táctil. Se utiliza para detectar cuando alguien acaricia al cachorro.
         self.touch_sensor = TouchSensor(Port.S1)
 
         self.pet_count_timer = StopWatch()
         self.feed_count_timer = StopWatch()
         self.count_changed_timer = StopWatch()
 
-        # These attributes are initialized later in the reset() method.
+        # Estos atributos se inicializan más tarde en el método reset().
         self.pet_target = None
         self.feed_target = None
         self.pet_count = None
         self.feed_count = None
 
-        # These attributes are used by properties.
+        # Estos atributos son utilizados por las propiedades.
         self._behavior = None
         self._behavior_changed = None
         self._eyes = None
         self._eyes_changed = None
 
-        # These attributes are used in the eyes update
+        # Estos atributos se utilizan en la actualización de los ojos
         self.eyes_timer_1 = StopWatch()
         self.eyes_timer_1_end = 0
         self.eyes_timer_2 = StopWatch()
         self.eyes_timer_2_end = 0
         self.eyes_closed = False
 
-        # These attributes are used by the playful behavior.
+        # Estos atributos son utilizados por el comportamiento lúdico.
         self.playful_timer = StopWatch()
         self.playful_bark_interval = None
 
-        # These attributes are used in the update methods.
+        # Estos atributos se utilizan en los métodos de actualización.
         self.prev_petted = None
         self.prev_color = None
 
     def adjust_head(self):
-        """Use the up and down buttons on the EV3 brick to adjust the puppy's
-        head up or down.
+        """Utiliza los botones arriba y abajo del ladrillo EV3 para ajustar la 
+        cabeza del cachorro hacia arriba o hacia abajo.
         """
         self.ev3.screen.load_image(ImageFile.EV3_ICON)
         self.ev3.light.on(Color.ORANGE)
@@ -108,37 +109,37 @@ class Puppy:
         self.ev3.light.on(Color.GREEN)
 
     def move_head(self, target):
-        """Move the head to the target angle.
+        """Mueve la cabeza al ángulo objetivo.
 
-        Arguments:
+        Argumentos:
             target (int):
-                The target angle in degrees. 0 is the starting position,
-                negative values are below this point and positive values
-                are above this point.
+                El ángulo objetivo en grados. 0 es la posición inicial,
+                valores negativos están por debajo de este punto y valores positivos
+                están por encima de este punto.
         """
         self.head_motor.run_target(20, target)
 
     def reset(self):
-        # must be called when puppy is sitting down.
+        # debe ser llamado cuando el cachorro está sentado.
         self.left_leg_motor.reset_angle(0)
         self.right_leg_motor.reset_angle(0)
-        # Pick a random number of time to pet the puppy.
+        # Elige un número aleatorio de tiempo para acariciar al cachorro.
         self.pet_target = urandom.randint(3, 6)
-        # Pick a random number of time to feed the puppy.
+        # Elige un número aleatorio de tiempo para alimentar al cachorro.
         self.feed_target = urandom.randint(2, 4)
-        # Pet count and feed count both start at 1
+        # El recuento de caricias y el recuento de alimentos comienzan en 1
         self.pet_count, self.feed_count = 1, 1
-        # Reset timers.
+        # Reinicia los temporizadores.
         self.pet_count_timer.reset()
         self.feed_count_timer.reset()
         self.count_changed_timer.reset()
-        # Set initial behavior.
+        # Establece el comportamiento inicial.
         self.behavior = self.idle
 
-    # The next 8 methods define the 8 behaviors of the puppy.
+    # Los siguientes 8 métodos definen los 8 comportamientos del cachorro.
 
     def idle(self):
-        """The puppy is idle and waiting for someone to pet it or feed it."""
+        """El cachorro esta ocioso y esperando a que alguien lo acaricie o le dé de comer""".
         if self.did_behavior_change:
             print('idle')
             self.stand_up()
@@ -148,7 +149,7 @@ class Puppy:
         self.update_feed_count()
 
     def go_to_sleep(self):
-        """Makes the puppy go to sleep."""
+        """Hace que el cachorro se duerma."""
         if self.did_behavior_change:
             print('go_to_sleep')
             self.eyes = self.TIRED_EYES
@@ -161,7 +162,7 @@ class Puppy:
             self.behavior = self.wake_up
 
     def wake_up(self):
-        """Makes the puppy wake up."""
+        """Hace que el cachorro se despierte."""
         if self.did_behavior_change:
             print('wake_up')
         self.eyes = self.TIRED_EYES
@@ -174,7 +175,7 @@ class Puppy:
         self.behavior = self.idle
 
     def act_playful(self):
-        """Makes the puppy act playful."""
+        """Hace que el cachorro actue de forma juguetona."""
         if self.did_behavior_change:
             print('act_playful')
             self.eyes = self.NEUTRAL_EYES
@@ -182,7 +183,7 @@ class Puppy:
             self.playful_bark_interval = 0
 
         if self.update_pet_count():
-            # If the puppy was petted, then we are done being playful
+            # Si el cachorro fue acariciado, entonces hemos terminado de ser juguetones
             self.behavior = self.idle
 
         if self.playful_timer.time() > self.playful_bark_interval:
@@ -191,7 +192,7 @@ class Puppy:
             self.playful_bark_interval = urandom.randint(4, 8) * 1000
 
     def act_angry(self):
-        """Makes the puppy act angry."""
+        """Hace que el cachorro se enfade."""
         if self.did_behavior_change:
             print('act_angry')
         self.eyes = self.ANGRY_EYES
@@ -211,11 +212,11 @@ class Puppy:
             self.ev3.speaker.play_file(SoundFile.DOG_WHINE)
 
         if self.update_feed_count():
-            # If we got food, then we are not longer hungry.
+            # Si tenemos comida, entonces ya no tenemos hambre.
             self.behavior = self.idle
 
         if self.update_pet_count():
-            # If we got a pet instead of food, then we are angry.
+            # Si nos dan una mascota en vez de comida, entonces nos enfadamos.
             self.behavior = self.act_angry
 
     def go_to_bathroom(self):
@@ -249,7 +250,7 @@ class Puppy:
         self.reset()
 
     def sit_down(self):
-        """Makes the puppy sit down."""
+        """Hace que el cachorro se siente."""
         self.left_leg_motor.run(-50)
         self.right_leg_motor.run(-50)
         wait(1000)
@@ -257,11 +258,11 @@ class Puppy:
         self.right_leg_motor.stop()
         wait(100)
 
-    # The next 4 methods define actions that are used to make some parts of
-    # the behaviors above.
+    # Los siguientes 4 métodos definen acciones que se utilizan para hacer algunas partes de
+    # los comportamientos anteriores.
 
     def stand_up(self):
-        """Makes the puppy stand up."""
+        """Hace que el cachorro se ponga de pie."""
         self.left_leg_motor.run_target(100, self.HALF_UP_ANGLE, wait=False)
         self.right_leg_motor.run_target(100, self.HALF_UP_ANGLE)
         while not self.left_leg_motor.control.done():
@@ -275,7 +276,7 @@ class Puppy:
         wait(500)
 
     def stretch(self):
-        """Makes the puppy stretch its legs backwards."""
+        """Hace que el cachorro estire las patas hacia atrás."""
         self.stand_up()
 
         self.left_leg_motor.run_target(100, self.STRETCH_ANGLE, wait=False)
@@ -291,7 +292,7 @@ class Puppy:
             wait(100)
 
     def hop(self):
-        """Makes the puppy hop."""
+        """Hace que el cachorro salte."""
         self.left_leg_motor.run(500)
         self.right_leg_motor.run(500)
         wait(275)
@@ -306,7 +307,7 @@ class Puppy:
 
     @property
     def behavior(self):
-        """Gets and sets the current behavior."""
+        """Obtiene y establece el comportamiento actual."""
         return self._behavior
 
     @behavior.setter
@@ -317,8 +318,8 @@ class Puppy:
 
     @property
     def did_behavior_change(self):
-        """bool: Tests if the behavior changed since the last time this
-        property was read.
+        """bool: Prueba si el comportamiento cambió desde la última vez que esta
+        propiedad fue leída.
         """
         if self._behavior_changed:
             self._behavior_changed = False
@@ -326,28 +327,28 @@ class Puppy:
         return False
 
     def update_behavior(self):
-        """Updates the :prop:`behavior` property based on the current state
-        of petting and feeding.
+        """Actualiza la propiedad :prop:`behavior` basándose en el estado actual
+        de acariciar y alimentar.
         """
         if self.pet_count == self.pet_target and self.feed_count == self.feed_target:
-            # If we have the exact right amount of pets and feeds, act happy.
+            # Si tenemos la cantidad exacta de caricias y alimentos, actuar feliz.
             self.behavior = self.act_happy
         elif self.pet_count > self.pet_target and self.feed_count < self.feed_target:
-            # If we have too many pets and not enough food, act angry.
+            # Si tenemos demasiadas caricias y poca comida, actúa con rabia.
             self.behavior = self.act_angry
         elif self.pet_count < self.pet_target and self.feed_count > self.feed_target:
-            # If we have not enough pets and too much food, go to the bathroom.
+            # Si no tenemos suficientes caricias y demasiada comida, vamos al baño.
             self.behavior = self.go_to_bathroom
         elif self.pet_count == 0 and self.feed_count > 0:
-            # If we have no pets and some food, act playful.
+            # Si no tenemos caricias y algo de comida, actúa juguetón.
             self.behavior = self.act_playful
         elif self.feed_count == 0:
-            # If we have no food, act hungry.
+            # Si no tenemos comida, actúa con hambre.
             self.behavior = self.act_hungry
 
     @property
     def eyes(self):
-        """Gets and sets the eyes."""
+        """Consigue y fija la mirada""".
         return self._eyes
 
     @eyes.setter
@@ -376,13 +377,13 @@ class Puppy:
                     self.eyes = self.TIRED_RIGHT_EYES
 
     def update_pet_count(self):
-        """Updates the :attr:`pet_count` attribute if the puppy is currently
-        being petted (touch sensor pressed).
+        """Actualiza el atributo :attr:`pet_count` si el cachorro está siendo
+        acariciado (sensor táctil pulsado).
 
-        Returns:
+        Devuelve:
             bool:
-                ``True`` if the puppy was petted since the last time this method
-                was called, otherwise ``False``.
+                ``True`` si el cachorro fue acariciado desde la última vez que este método fue llamado.
+                 en caso contrario ``False``.
         """
         changed = False
 
@@ -400,13 +401,13 @@ class Puppy:
         return changed
 
     def update_feed_count(self):
-        """Updates the :attr:`feed_count` attribute if the puppy is currently
-        being fed (color sensor detects a color).
+        """Actualiza el atributo :attr:`feed_count` si el cachorro está siendo
+        alimentado (el sensor de color detecta un color).
 
-        Returns:
+        Devuelve:
             bool:
-                ``True`` if the puppy was fed since the last time this method
-                was called, otherwise ``False``.
+                ``True`` si el cachorro fue alimentado desde la última vez que este método fue llamado.
+                 de lo contrario ``False``.
         """
         color = self.color_sensor.color()
         changed = False
@@ -423,7 +424,7 @@ class Puppy:
         return changed
 
     def monitor_counts(self):
-        """Monitors pet and feed counts and decreases them over time."""
+        """Supervisa los recuentos de caricias y alimentos y los reduce con el tiempo"""
         if self.pet_count_timer.time() > 15000:
             self.pet_count_timer.reset()
             self.pet_count = max(0, self.pet_count - 1)
@@ -433,12 +434,12 @@ class Puppy:
             self.feed_count = max(0, self.feed_count - 1)
             print('feed_count:', self.feed_count, 'feed_target:', self.feed_target)
         if self.count_changed_timer.time() > 30000:
-            # If nothing has happened for 30 seconds, go to sleep
+            # Si no ha pasado nada durante 30 segundos, ir a dormir
             self.count_changed_timer.reset()
             self.behavior = self.go_to_sleep
 
     def run(self):
-        """This is the main program run loop."""
+        """Este es el bucle de ejecución del programa principal."""
         self.sit_down()
         self.adjust_head()
         self.eyes = self.SLEEPING_EYES
@@ -449,7 +450,7 @@ class Puppy:
             wait(100)
 
 
-# This covers up the tear to make a new image.
+# Esto cubre la lágrima para hacer una nueva imagen.
 Puppy.SQUINTY_EYES.draw_box(120, 60, 140, 85, fill=True, color=Color.WHITE)
 
 
